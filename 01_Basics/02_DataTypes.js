e388d89b91109a5e8d71991ade541ad98f8502e1
SOUNAK GHOSH <sounak2309@gmail.com>
2024-07-05 09:20:39 +0530

    datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // this cannot be executes as we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh") //good readability
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number = 2 ^ 53
+// bigint = for large numbers
+// string = "assembly of characters"
+// boolean = true or false
+// null = stand alone value (type = object)
+// undefined = kinda place holder for a value which is not defined yet (type = undefined)
+// symbol = uniquness
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
